69093227d158174a71610c06bef3000c344eee98
Washington Junio C. Rodrigues <washingtonjunio16@gmail.com>
2021-01-20 19:07:02 -0300

    Filtro rota inválida

---
+++ api/src/app.filter.ts
@@ -0,0 +1,21 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class AppFilter<T> implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    response.status(HttpStatus.NOT_FOUND).json({
+      message: 'Endpoint não encontrado',
+    });
+  }
+}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { AppFilter } from './app.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const { PORT } = process.env;
+  const app = await NestFactory.create(AppModule, { cors: true });
 
+  app.setGlobalPrefix('api');
+  app.useGlobalFilters(new AppFilter());
+
+  const { PORT } = process.env;
   await app.listen(PORT || 3000);
 }
 bootstrap();
